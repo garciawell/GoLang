4d52d38d3d088b6d6cbb16b63d51e9a4b16e6ccd
Wellington Garcia <garciawell@gmail.com>
2021-11-07 13:04:11 -0300

    Slices and For Loops

--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	// var card string = "Ace of Spades"
-	card := "Ace of Spades"
-	card = newCard()
 
-	fmt.Println(card)
+	cards := []string{"Ace of Diamonds", newCard()}
+	cards = append(cards, "Six of Spades")
+
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
 }
 
 func newCard() string {
