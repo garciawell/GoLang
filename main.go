51e165b1d6f95c16e46dc8ab296c54a48cb69dde
Wellington Garcia <wellington.garcia@bancointer.com.br>
2021-11-15 21:22:54 -0300

    Random Number Generation

--- main.go
+++ main.go
@@ -10,6 +10,11 @@ func main() {
 	cards := newDeck()
 	cards.saveToFile("my_cards")
 
-	cards2 := newDeckFromFile("my_cards")
-	cards2.print()
+	// cards2 := newDeckFromFile("my_cards")
+	// cards2.print()
+
+	cards3 := newDeck()
+	cards3.shuffle()
+	cards3.print()
+
 }
